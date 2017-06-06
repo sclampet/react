0469e416507c53239be5646fbfee74d56cecb5fa
scottviewzr <scott@viewzr.com>
2017-06-06 12:12:54 -0400

    complete hello-world

---
+++ project1/hello-world/src/Intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code />src/App.js and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+export default Intro;

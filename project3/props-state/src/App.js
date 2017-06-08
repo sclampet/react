f95f94ac262b3c96cfb8454939aedd72d1c39323
scottviewzr <scott@viewzr.com>
2017-06-08 12:02:39 -0400

    learn propTypes basics

--- project3/props-state/src/App.js
+++ project3/props-state/src/App.js
@@ -13,11 +13,29 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>propNumber is: { this.props.propNumber }</h3>
+        <h3>propObject is: { this.props.propObject.obj1 }</h3>
+        <h3>propString is: { this.props.propString }</h3>
         <Parent />
       </div>
     );
   }
 }
+
+
+App.propTypes = {
+  propObject: React.PropTypes.object,
+  propString: React.PropTypes.string,
+  propNumber: React.PropTypes.number,
+  propArray: React.PropTypes.array,
+}
+
+App.defaultProps = {
+  propNumber: 3,
+  propString: 'This is prop string',
+  propObject: {obj1: 'Obj1', obj2: 'Obj2', obj3: 'Obj3'}
+}
+
 
 class Parent extends Component {
   render() {
@@ -29,6 +47,7 @@ class Parent extends Component {
     );
   }
 }
+
 
 Parent.defaultProps = {
   cars: ['BMW', 'Ford', 'Toyota'],

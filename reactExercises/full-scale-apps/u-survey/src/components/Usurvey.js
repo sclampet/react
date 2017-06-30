e945dfc6d877d0fb3e5589e8dbe9665032b71917
scottviewzr <scott@viewzr.com>
2017-06-30 11:57:44 -0600

    initial u-survey setup

---
+++ reactExercises/full-scale-apps/u-survey/src/components/Usurvey.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Usurvey extends Component {
+  render() {
+    return (
+      <div>
+        Usurvey Component
+      </div>
+    );
+  }
+}
+export default Usurvey;

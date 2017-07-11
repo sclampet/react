080555c3024e70df78be9fe5804d716fbdb89eea
scottviewzr <scott@viewzr.com>
2017-07-11 12:47:13 -0600

    add signup/logout functionality

--- reactExercises/full-scale-apps/firebase-login/src/comps/Auth.js
+++ reactExercises/full-scale-apps/firebase-login/src/comps/Auth.js
@@ -14,19 +14,55 @@ firebase.initializeApp(config);
 
 class Auth extends Component {
 
-	login(event) {
+	login() {
 		const email = this.refs.email.value;
 		const password = this.refs.password.value;
 		// console.log(email, password)
 		const auth = firebase.auth();
 		const promise = auth.signInWithEmailAndPassword(email, password);
 
-		// TODO: handle login promise
-		promise.catch(e => {
-			var err = e.message;
-			console.log(err);
-			this.setState({err: err});
-		});
+		promise
+			.then(user => {
+				var logout = document.getElementById('logout');
+				logout.classList.remove('hide');
+				this.setState({err: 'Welcome '+user.email});
+			})
+			.catch(e => {
+				var err = e.message;
+				console.log(err);
+				this.setState({err: err});
+			});
+	};
+
+	logout() {
+		firebase.auth().signOut();
+
+		var logout = document.getElementById('logout');
+		logout.classList.add('hide'); 
+		this.setState({err: 'Goodbye!'});
+	};
+
+	signup() {
+		const email = this.refs.email.value;
+		const password = this.refs.password.value;
+		// console.log(email, password)
+
+		const auth = firebase.auth();
+		const promise = auth.createUserWithEmailAndPassword(email, password);
+
+		promise
+			.then(user => {
+				var err = "Welcome "+user.email;
+				firebase.database().ref('/users'+user.uid).set({
+					email: user.email,
+				});
+				console.log('user: ', user)
+				this.setState({err: err});
+			})
+			.catch(e => {
+				var err = e.message;
+				this.setState({err: err});
+			})
 	}
 
 	constructor(props) {
@@ -36,6 +72,8 @@ class Auth extends Component {
 		}
 
 		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
+		this.signup = this.signup.bind(this);
 	}
 
 	render() {
@@ -46,8 +84,8 @@ class Auth extends Component {
 				<input id='email' ref='email' type='email' placeholder='email' /><br />
 				<input id='password' ref='password' type='password' placeholder='password' /><br />
 				<button onClick={this.login}>LOGIN</button>
-				<button>SIGN-UP</button>
-				<button>LOGOUT</button>
+				<button onClick={this.signup}>SIGN-UP</button>
+				<button id='logout' className='hide' onClick={this.logout}>LOGOUT</button>
 			</div>
 		);
 	}

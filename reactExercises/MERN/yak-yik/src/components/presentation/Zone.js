471f8c540f937a007ea3afe54745c7909eb19ba0
Scott Clampet <scottclampet@Scotts-MacBook-Pro.local>
2017-08-24 11:23:50 -0600

    Begin API manager lesson

--- reactExercises/MERN/yak-yik/src/components/presentation/Zone.js
+++ reactExercises/MERN/yak-yik/src/components/presentation/Zone.js
@@ -6,11 +6,11 @@ class Zone extends Component {
     	const style = styles.zone;
         return (
             <div>
-        		<div style={style.container}>
-        			<h2 style={style.h2}><a style={style.a} href='#'>{this.props.currentZone.name}</a></h2>
-        			<span className='detail'>{this.props.currentZone.zipCode}</span><br />
-        			<span className='detail'>Comments: {this.props.currentZone.numComments}</span>
-        		</div>
+          		<div style={style.container}>
+          			<h2 style={style.h2}><a style={style.a} href='#'>{this.props.currentZone.name}</a></h2>
+          			<span className='detail'>{this.props.currentZone.zipCode}</span><br />
+          			<span className='detail'>Comments: {this.props.currentZone.numComments}</span>
+          		</div>
             </div>
         );
     }
